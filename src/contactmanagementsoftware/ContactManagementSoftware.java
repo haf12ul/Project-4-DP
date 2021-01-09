eae7bdf612c163b5b9546024729aa833e878021d
Waniey951215 <wanieytaewonnie@yahoo.com>
2021-01-09 21:15:31 +0800

    //addcomment

--- src/contactmanagementsoftware/ContactManagementSoftware.java
+++ src/contactmanagementsoftware/ContactManagementSoftware.java
@@ -7,5 +7,6 @@ public class ContactManagementSoftware implements Serializable {
     public static void main(String[] args) {
         MUI mg = MUI.getInstance();
         mg.setVisible(true);
+        mg.setLocationRelativeTo(null);
     }
 }
